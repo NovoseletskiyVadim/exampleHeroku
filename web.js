40fd3b208a31db9bedb8be137735973e68b2d0ae
NovoseletskiyVadim <novoseletskiy.v@gmail.com>
2019-07-09 13:47:57 +0300

    Fix bug

--- web.js
+++ web.js
@@ -1,6 +1,16 @@
-var express = require('express');
-var app = express();
-var path = require('path');
-app.use(express.static(path.join(process.cwd(), 'source')));
-console.log(process.env.PORT || 5000);
-app.listen(process.env.PORT || 5000);
+// var express = require('express');
+// var app = express();
+// var path = require('path');
+// app.use(express.static(path.join(process.cwd(), 'source')));
+// console.log(process.env.PORT || 5000);
+// app.listen(process.env.PORT || 5000);
+
+const http=require('http');
+const expess=require('express');
+const app=expess();
+
+app.use(expess.static('.'));
+
+http.createServer(app).listen(3000, function () {
+    console.log('listening 3000 port');
+  });
